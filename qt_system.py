ea4b6357609aa6d505b8062daeb67bd4941d7b10
R1das67 <heinrichleon09@gmail.com>
2025-10-14 21:12:14 +0200

    qt_system.py

--- qt_system.py
+++ qt_system.py
@@ -9,6 +9,7 @@ from discord import AllowedMentions
 intents = discord.Intents.default()
 intents.guilds = True
 intents.members = True
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="$", intents=intents)
 
